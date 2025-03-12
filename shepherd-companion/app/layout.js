5acd79c22c31ff8a197d3967419162360ff678da
Armstrong2035 <armstrongolusoji9@gmail.com>
2025-03-12 17:03:07 +0400

    Removed unnecesary stuff

--- shepherd-companion/app/layout.js
+++ shepherd-companion/app/layout.js
@@ -1,19 +1,12 @@
-import { Inter } from "next/font/google";
-import ClientRootLayout from "./ClientRootLayout";
-
-const inter = Inter({ subsets: ["latin"] });
-
 export const metadata = {
-  title: "Shepherd CRM Companion",
+  title: "Shepherd Companion",
   description: "The companion app for Shepherd CRM for church teams",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <ClientRootLayout>{children}</ClientRootLayout>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
--- shepherd-companion/app/page.js
+++ shepherd-companion/app/page.js
@@ -1,33 +1,7 @@
-"use client";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-import { CircularProgress, Box } from "@mui/material";
-import { useAuth } from "@/context/AuthContext";
-
-export default function Home() {
-  const { user, loading } = useAuth();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (!loading) {
-      if (user) {
-        router.push("/dashboard");
-      } else {
-        router.push("/login");
-      }
-    }
-  }, [user, loading, router]);
-
+export default function Page() {
   return (
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-      }}
-    >
-      <CircularProgress />
-    </Box>
+    <>
+      <p>Main companion page</p>
+    </>
   );
 }
